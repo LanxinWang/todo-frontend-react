0e78b597c733637523ff5cd6e5261df8aba72993
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-09-19 11:00:06 +0800

    finish todo app header and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="TODO">
-        A
+      <header id='title'><h1>todos</h1></header>
+      <div id='todo-app'>
+        <todo-add />
+        <todo-list />
+        <todo-count />
+      </div>
+      <footer id='info'>
+        <p>Double-click to edit a todo</p>
+          <p>Create by <a href="http://github.com/petehunt/">petehunt</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer> 
     </div>
   );
 }
