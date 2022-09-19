065145d67b3a888451c3a05593d08d9d5ec030b7
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-09-19 16:14:55 +0800

    finish add new todo logic

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import './component/TodoAdd';
 import TodoAdd from './component/TodoAdd';
 import TodoList from './component/TodoList';
 import TodoCount from './component/TodoCount';
-
+import { useEffect,useState } from 'react';
+import TODO_STATUS, { TITLE } from './constantValue';
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(()=>{
+    console.log(todos);
+  },[todos])
+
+  const addTodoItem = (content) => {
+    setTodos([{
+      index,
+      status:TODO_STATUS.ACTIVE,
+      content
+    },...todos]);
+    setIndex(index + 1 );
+  }
+
+  const toggleAll = (toggleFlag) => {
+    setTodos(todos.map(todo =>{
+      todo.status = toggleFlag ? TODO_STATUS.COMPLETED : TODO_STATUS.ACTIVE
+      return todo;
+    }))
+  }  
+
   return (
     <div className="TODO">
-      <header id='title'><h1>todos</h1></header>
+      <header id='title'><h1>{TITLE}</h1></header>
       <div id='todo-app'>
-        <TodoAdd />
-        <TodoList />
-        <TodoCount />
+        <TodoAdd getNewItem = {addTodoItem} toggleAll={toggleAll} todoLength={todos.length}/>
+        <TodoList {...todos}/>
+        <TodoCount {...todos}/>
       </div>
       <footer id='info'>
         <p>Double-click to edit a todo</p>
--- src/component/TodoAdd/index.js
+++ src/component/TodoAdd/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './index.css';
 
-function TodoAdd() {
+const TodoAdd = ({getNewItem,toggleAll,todoLength}) => {
+    const handleCheck = ({target}) =>{
+        toggleAll(target.checked)
+    }
+    
+    const handleOnKeyDown =(event) =>{
+        let content = event.target.value.trim();
+        if (event.keyCode===13 && content !== '') {
+            getNewItem(content);
+            event.target.value = '';
+        }
+    }
+    
     return (
         <div id='add-todo'>
-             <input id="toggle-all" type="checkbox" />
-             <label htmlFor="toggle-all">❯</label>
-            <input id="new-todo" placeholder="What needs to be done?"></input>
+            <input id="toggle-all" type="checkbox" onChange={handleCheck} />
+            <label htmlFor="toggle-all" style={{ visibility: todoLength > 0 ? 'visible' : 'hidden' }}>❯</label>
+            <input id="new-todo" placeholder="What needs to be done?" onKeyDown={handleOnKeyDown}></input>
         </div>
     )
 }
