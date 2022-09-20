da3800925696813de5d7516df7c459bcf79b22e3
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-09-20 16:49:08 +0800

    finish todo count

--- src/component/TodoCount/index.js
+++ src/component/TodoCount/index.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import TODO_STATUS from "../../constantValue";
+import "./index.css";
 
-function TodoCount() {
-    return (
-        <div id="count-todo">
-            <span id="active-count">
-                <strong></strong>
-                <span> items</span>
-                <span> left</span>
-            </span>
-            <ul id="filters">
-                <li><button id="all" className='selected'>All</button></li>
-                <li><button id="active">Active</button></li>
-                <li><button id="completed">Completed</button></li>
-            </ul>
-            <button id="clear-completed">Clear completed</button>
-        </div>
-    )
+const TODO_STATUS_OPTION_ALL = "all";
+const CLASS_SELEVTED = "selected";
+const todoStatusOptions = [
+  TODO_STATUS_OPTION_ALL,
+  TODO_STATUS.ACTIVE,
+  TODO_STATUS.COMPLETED,
+];
+
+function TodoCount({
+  todos,
+  getTodoStatus,
+  selectedTodoStatus,
+  clearCompletedTodo,
+}) {
+  const handleClick = (event) => {
+    event.target.className = CLASS_SELEVTED;
+    getTodoStatus(event.target.id);
+  };
+
+  return (
+    <div
+      id="count-todo"
+      style={{ display: todos.length > 0 ? "flex" : "none" }}
+    >
+      <span id="active-count">
+        <strong>
+          {todos.filter((todo) => todo.status === TODO_STATUS.ACTIVE).length}
+        </strong>
+        <span> items left</span>
+      </span>
+
+      <ul id="filters">
+        {todoStatusOptions.map((statusOption) => (
+          <li key={statusOption}>
+            <button
+              id={statusOption}
+              onClick={handleClick}
+              className={statusOption === selectedTodoStatus ? "selected" : ""}
+            >
+              {statusOption}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button id="clear-completed" onClick={clearCompletedTodo}>
+        Clear completed
+      </button>
+    </div>
+  );
 }
 
 export default TodoCount;
