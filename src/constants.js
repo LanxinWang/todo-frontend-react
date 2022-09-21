197d9b93f4879d279fff87314916d5974edfcc41
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-09-21 14:05:40 +0800

    add a constant value for todo menu

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const ENTER_KEY = 13;
+export const TITLE = "Todos";
+const TODO_STATUS = {
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
+export const TODO_MENU = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
+
+export default TODO_STATUS;
