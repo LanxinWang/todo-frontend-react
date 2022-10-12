0c704e5588a45da3037bdc5b974348fec75e8eb9
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-10-12 10:56:58 +0800

    refactor as ts file

---
+++ src/constants/constants.tsx
@@ -0,0 +1,19 @@
+export const ENTER_KEY = "Enter";
+export const TITLE = "Todos";
+export const TODO_STATUS = {
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
+export const TODO_MENU = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
+
+export type Todo ={
+  id: number,
+  status: string,
+  name: string,
+}
+
+export default TODO_STATUS;
