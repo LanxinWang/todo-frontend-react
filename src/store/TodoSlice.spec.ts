d1c48f042f22b875e7f8b979e3522eb6c6b032b6
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-10-19 13:06:19 +0800

    add a test about deleteTodo

--- src/store/TodoSlice.spec.ts
+++ src/store/TodoSlice.spec.ts
@@ -1,23 +1,37 @@
-import todoReducer, {todoState, createTodo} from "./TodoSlice";
+import todoReducer, {todoState, createTodo, deleteTodo} from "./TodoSlice";
 
-const mockTodo = {
-    id: "test-id",
-    status: "test-status",
+const mockTodo = [{
+    id: "0",
+    status: "active",
     name: "todo1"
-}
+},
+{
+    id: "1",
+    status: "active",
+    name: "todo2"
+}]
 describe("todo reducer",()=>{
     const initialState: todoState = {
-        todoList: [mockTodo]
+        todoList: [...mockTodo]
       };
     it("should handle initial state",()=>{
         expect(todoReducer(undefined, { type: 'unknown' })).toEqual({
             "todoList": []
         });
-    })
+    });
     it("should create todo for name",()=>{
-        const {todoList} = todoReducer(initialState, createTodo({name:"todo2"}));
-        expect(todoList.length).toBe(2);
-        expect(todoList[0].name).toEqual("todo2")
+        const {todoList} = todoReducer(initialState, createTodo({name:"todo3"}));
+        expect(todoList.length).toBe(3);
+        expect(todoList[0].name).toEqual("todo3")
 
-    })
+    });
+    it("should delete todo for id",()=>{
+        const {todoList} = todoReducer(initialState, deleteTodo({id:"0"}));
+        expect(todoList).toEqual([{
+            id: "1",
+            status: "active",
+            name: "todo2"
+        }])
+    });
+
 })  
