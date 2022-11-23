1561b7e26d8263434d1d7a9c4c8c86fd60039ed7
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-23 21:10:51 +0800

    create apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'https://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+
 );
