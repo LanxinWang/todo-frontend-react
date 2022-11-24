1177f1fe8ddfeaf4a92cfa8ba3997bff4ae9dd41
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-24 11:49:33 +0800

    refactor inline function

--- src/component/TodoHeader/index.tsx
+++ src/component/TodoHeader/index.tsx
@@ -26,28 +26,18 @@ const TodoHeader =({
   const [ updateAllTodoStatus ] = useMutation(UPDATE_ALL_TODOS);
   const [name,setName] = useState("")
 
-  const addTodo = (name: string) => {
-    addATodo({ variables: { id: `${todos.length}`, status: TODO_STATUS.ACTIVE, name } });
-  };
-  const toggleAllTodos = (checkFlag: boolean) => {
+  const handleLabelChange = (checkFlag: boolean) => {
     const updateIds = todos.filter(todo => todo.status !== TODO_STATUS.DELETED).map(todo => `${todo._id}`);    
     updateAllTodoStatus( { variables: {updateIds, isChecked: checkFlag }} )
-  };
-
-  const handleLabelChange = (checkFlag: boolean) => {
-    toggleAllTodos(checkFlag);
   };
   const handleKeyDown = (key: string) => {
     if (key === ENTER_KEY) {
       if (name.trim() !== "") {
-        addTodo(name); 
+        addATodo({ variables: { id: `${todos.length}`, status: TODO_STATUS.ACTIVE, name } }); 
       }
       setName("");
     }
   };
-  const handleInputChange = (name: string) => {
-    setName(name);
-  }
 
   return (
     <TodoHeaderContainer>
@@ -68,7 +58,7 @@ const TodoHeader =({
         <NewTodoInput 
         id="new-todo-input" 
         value={name}
-        onChange={(e)=>{handleInputChange(e.target.value)}}
+        onChange={(e)=>{setName(e.target.value)}}
         onKeyDown={(e) => handleKeyDown(e.key)} />
       {/* </form> */}
 
